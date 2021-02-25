f39b9b5d9b5040112cddb7b917265df9eb1d0527
Guilherme Solinscki <guilherme.slnk@gmail.com>
2021-02-25 19:04:12 -0300

    Add selected items containers

--- app/javascript/components/containers/TradingContainer.js
+++ app/javascript/components/containers/TradingContainer.js
@@ -1,16 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import SelectedItemsContainer from "../containers/SelectedItemsContainer";
+
 class TradingContainer extends React.Component {
   render () {
     return (
       <React.Fragment>
         {this.props.title}
+        <SelectedItemsContainer items={this.props.items} selectedItems={[]}></SelectedItemsContainer>
+        <SelectedItemsContainer items={this.props.items} selectedItems={[]}></SelectedItemsContainer>
       </React.Fragment>
     );
   }
 }
 
 TradingContainer.propTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
+  items: PropTypes.array
 };
 export default TradingContainer
