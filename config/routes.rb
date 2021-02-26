d82c57c200665112e4a569d5ab26cdff28d880e6
Guilherme Solinscki <guilherme.slnk@gmail.com>
2021-02-26 15:37:46 -0300

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/", to: "home#index"
+
+  get "/pokemons", to: "pokemon#get"
+
 end
